5c07f59149b289719475aa086a5fbe13f4463708
@ivan.t202406 <ivan.t202406@gmail.com>
2024-12-08 15:37:17 -0400

    segundo commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,11 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-
-  const cookies = request.cookies.get('datosUsuario');
+  const cookies = request.cookies.get("datosUsuario");
 
   if (!cookies) {
-
-    return NextResponse.redirect(new URL('/', request.url))
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   const datosUsuario = JSON.parse(cookies.value);
@@ -16,19 +14,30 @@ export function middleware(request: NextRequest) {
   const usuarioRol = datosUsuario.rol?.rol;
 
   if (!token) {
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
-  const protegerAdminRutas = ['/panel/administracion/usuarios', '/panel/administracion', '/panel/administracion/reportes'];
+  const protegerAdminRutas = [
+    "/panel/administracion/usuarios",
+    "/panel/administracion",
+    "/panel/administracion/reportes",
+  ];
   const rutaActual = new URL(request.url).pathname;
 
-  if (protegerAdminRutas.includes(rutaActual) && usuarioRol !== 'Administrador') {
-    return NextResponse.redirect(new URL('/panel/noautorizado', request.url));
+  if (
+    protegerAdminRutas.includes(rutaActual) &&
+    usuarioRol !== "Administrador"
+  ) {
+    return NextResponse.redirect(new URL("/panel/noautorizado", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/panel/administracion/usuarios', '/panel/administracion', '/panel/administracion/reportes', '/panel/marcarparda/final', '/panel/marcarparda/inicio'],
-}
+  matcher: [
+    "/panel/administracion/usuarios",
+    "/panel/administracion",
+    "/panel/administracion/reportes",
+  ],
+};
